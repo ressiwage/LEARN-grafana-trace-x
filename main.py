877ca949b1961d184f8e17d37efc1c56b8545fd3
rswg <ressiwage@ya.ru>
2025-07-17 15:05:58 +0500

    forgot update path

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 import datetime
 
-sqlite_file_name = "main.db"
+sqlite_file_name = "/home/grafana/main.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
